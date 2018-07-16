f92c53f74a57d4c013233d9015ad7fde035224e3
Bruna Alves <brunahalves94@gmail.com>
2018-07-16 14:04:43 -0300

    Rotas, Parâmetros

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () { //função anomima que sera executada cada vez que o usuário executar a rota
     return view('welcome');
+});
+
+Route::get('/ola', function () { //utilização simples das rotas
+    return "<h1>Seja Bem vindo!</h1>";
+});
+
+Route::get('/ola/sejabemvindo', function () { //utilização simples das rotas
+    return "<h1>Olá, visitante! Seja Bem vindo!</h1>";
+});
+
+Route::get('/nome/{nome}/{sobrenome}', function($nome, $sn){ //{} é passado o parâmetro que é o nome da pessoa, no exemplo. Estamos nomeando um parametro que vai ser recebido. Toda vez que passar um parametro, deverar ser passado também na função
+    return "<h1>Olá, $nome $sn!</h1>"; // isso será passado no browser, por exemplo: http://localhost:8000/nome/bruna/alves
+
+});
+
+
+Route::get('/repetir/{nome}/{n}', function($nome, $n){ // vai repetir o nome a quantidade de vezes que for passar no parametro n. Espera receber os 3 níveis, ou a rota não será encontrada. Não está restrito então os parametros recebem qualquer coisa
+    if(is_integer($n)){ //o parametro n precisa ser inteiro nesse caso, por causa do proximo for
+        for($i=0; $i<$n; $i++){
+            echo "<h1>Olá, $nome!</h1>"; // exemplo browser: http://localhost:8000/repetir/bruna/10
+        };
+    }
+    else
+        echo "Você não digitou um inteiro";   
 });
