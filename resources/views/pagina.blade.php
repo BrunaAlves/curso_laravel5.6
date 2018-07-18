2d9d9334e802a02c6d7555b75382d37b4f302024
Bruna Alves <brunahalves94@gmail.com>
2018-07-18 09:00:27 -0300

    View's: Preparando projeto para o Bootstrap 4

---
+++ resources/views/pagina.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{URL::to('css/app.css')}}" rel="stylesheet">
+    <!-- <link href="{{asset('css/app.css')}}" rel="stylesheet"> -->
+</head>
+<body>
+    <div class="alert alert-primary" role="alert">
+        This is a primary alert—check it out!
+    </div>
+    <script src="{{ URL:: to('js/app.js')}}" type="text/javascript"></script>
+    <!-- <script src="{{asset('js/app.js')}}" type="text/javascript"></script> -->
+</body>
+</html>
