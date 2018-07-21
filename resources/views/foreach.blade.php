bbd352bd5aa980dee52e18b10bfe91eb91317875
Bruna Alves <brunahalves94@gmail.com>
2018-07-21 13:31:20 -0300

    View's: Atributos dos laços

--- resources/views/foreach.blade.php
+++ resources/views/foreach.blade.php
@@ -1,5 +1,33 @@
-<h1>Loop Foreach - Array Associativo</h1>
+@extends('layout.meulayout')
 
-@foreach($produtos as $p)
-    <p>{{ $p['id'] }}: {{ $p['nome'] }} </p>
-@endforeach
+@section('minha_secao_produtos')
+
+    <h1>Loop Foreach - Array Associativo</h1>
+
+    @foreach($produtos as $p)
+        <p>{{ $p['id'] }}: {{ $p['nome'] }} </p>
+    @endforeach
+
+    <hr>
+
+    @foreach($produtos as $p)
+        <p>
+            {{ $p['id'] }}: {{ $p['nome'] }} 
+            @if ($loop->first)
+                (primeiro)
+            @endif
+            @if ($loop->last)
+                (Ultimo)
+            @endif
+            <span class="badge badge-secondary">
+                {{$loop->index}} / {{$loop->count-1}} / {{$loop->remaining}}
+            </span>
+
+            <span class="badge badge-warning">
+                {{$loop->iteration}} / {{$loop->count}}
+            </span>
+        
+        </p>
+    @endforeach
+
+@endsection
