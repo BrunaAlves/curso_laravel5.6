1063d456aab2cf30405b7dfe3d001b29eab6cbe8
Bruna Alves <brunahalves94@gmail.com>
2018-07-25 22:40:49 -0300

    Validação de formulários: Customização de mensagens - campo a campo

--- resources/views/novocliente.blade.php
+++ resources/views/novocliente.blade.php
@@ -28,19 +28,39 @@
                             @csrf
                             <div class="form-group">
                                 <label for="nome">Nome do Cliente</label>
-                                <input type="text" id="nome" class="form-control" name="nome" placeholder="Nome do Cliente">
+                                <input type="text" id="nome" class="form-control {{ $errors->has('nome') ? 'is-invalid' : '' }}" name="nome" placeholder="Nome do Cliente">
+                                @if($errors->has('nome'))
+                                    <div class="invalid-feedback">
+                                    {{ $errors->first('nome')}}
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="idade">Idade do Cliente</label>
-                                <input type="text" id="idade" class="form-control" name="idade" placeholder="idade do Cliente">
+                                <input type="text" id="idade" class="form-control {{ $errors->has('idade') ? 'is-invalid' : '' }}" name="idade" placeholder="idade do Cliente">
+                                @if($errors->has('idade'))
+                                    <div class="invalid-feedback">
+                                    {{ $errors->first('idade')}}
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="endereco">Endereço do Cliente</label>
-                                <input type="text" id="idade" class="form-control" name="endereco" placeholder="Endereço do Cliente">
+                                <input type="text" id="endereco" class="form-control {{ $errors->has('endereco') ? 'is-invalid' : '' }}" name="endereco" placeholder="Endereço do Cliente">
+                                @if($errors->has('endereco'))
+                                    <div class="invalid-feedback">
+                                    {{ $errors->first('endereco')}}
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="email">Email do Cliente</label>
-                                <input type="text" id="email" class="form-control" name="email" placeholder="Email do Cliente">
+                                <input type="text" id="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" placeholder="Email do Cliente">
+                                @if($errors->has('email'))
+                                    <div class="invalid-feedback">
+                                    {{ $errors->first('email')}}
+                                    </div>
+                                @endif
                             </div>
                             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                             <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
