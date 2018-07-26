847294686ff530ef6e3ad3b775722dbf0b76e6a1
Bruna Alves <brunahalves94@gmail.com>
2018-07-26 12:37:14 -0300

    Criando API e acessando via JQuery/Ajax: HTML - View e lista Produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -4,7 +4,7 @@
     <div class="card border">
         <div class="card-body">
             <h5 class="card-title">Cadastro de Produtos</h5>
-            @if(count($prods) > 0)
+
                 <table class="table table-ordered table-hover">
                     <thread>
                         <tr>
@@ -17,29 +17,15 @@
                         </tr>
                     </thread>
                     <tbody>
-                        @foreach($prods as $prod)
                             <tr>
-                                <td>{{$prod->id}}</td>
-                                <td>{{$prod->nome}}</td>
-                                <td>{{$prod->estoque}}</td>
-                                <td>{{$prod->preco}}</td>
-                                <td>{{$prod->categoria_id}}</td>
-                                <td>
-                                    <a href="/produtos/editar/{{$prod->id}}" class="btn btn-sm btn-primary">Editar</a>
-                                    <a href="/produtos/apagar/{{$prod->id}}" class="btn btn-sm btn-danger">Apagar</a>
-                                </td>
+                            
                             </tr>
-                        @endforeach
                     </tbody>
                 </table>
-            @endif
         </div>
         <div class="card-footer">
-            <a href="/produtos/novo" class="btn btn-sm btn-primary" role="button">Novo produto</a>
+            <button class="btn btn-sm btn-primary" role="button">Novo produto</a>
         </div>
     </div>
-
-
-    
 
 @endsection
