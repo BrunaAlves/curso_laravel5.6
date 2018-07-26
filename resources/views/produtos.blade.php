dd74752c29216d6dc6e9bcf590d6f8cc66fd9d5d
Bruna Alves <brunahalves94@gmail.com>
2018-07-26 13:28:33 -0300

    Criando API e acessando via JQuery/Ajax: Formulário para Salvar Produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -4,7 +4,6 @@
     <div class="card border">
         <div class="card-body">
             <h5 class="card-title">Cadastro de Produtos</h5>
-
                 <table class="table table-ordered table-hover">
                     <thread>
                         <tr>
@@ -28,4 +27,48 @@
         </div>
     </div>
 
+<div class="modal" tabindex="-1" role="dialog" id="dlgProdutos">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form class="form-horizontal" id="formProduto">
+            @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title"> Novo produto</h5>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" id="id" class="form-control">
+                    <div class="form-group">
+                        <label for="nomeProduto" class="control-label"> Nome do Produto</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="precoProduto" class="control-label"> Preço</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="precoProduto" placeholder="Preço do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantidadeProduto" class="control-label"> Quantidade</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="quantidadeProduto" placeholder="Quantidade do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="categoriaProduto" class="control-label"> Nome da categoria</label>
+                        <div class="input-group">
+                            <select class="form-control" id="departamentoProduto">
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <button type="cancel" class="btn btn-secondary" data-dissmiss="modal">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
