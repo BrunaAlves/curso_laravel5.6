a755f686b454094ecd41c633c7a093449f6ce492
Bruna Alves <brunahalves94@gmail.com>
2018-08-01 13:03:22 -0300

    Eloquente ORM - Relacionamentos UM PRA UM: hasOne mais detalhes

--- app/Cliente.php
+++ app/Cliente.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     public function endereco(){
-        return $this->hasOne('App\Endereco');
+        return $this->hasOne('App\Endereco', 'cliente_id', 'id');
     }
 }
