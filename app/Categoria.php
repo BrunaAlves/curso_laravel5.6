33355e4c79d2c615699b340c0629778c7c8cbf0a
Bruna Alves <brunahalves94@gmail.com>
2018-08-02 10:53:27 -0300

    Relacionamento UM PRA MUITOS: Utilizando o hasMany

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    function produtos(){
+        return $this->hasMany('App\Produto');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -697,3 +697,25 @@ Route::get('/produtos', function(){
         }
     }
 });
+
+Route::get('/categoriasprodutos', function(){
+    $cats = Categoria::all();
+    if(count($cats) === 0)
+        echo "<h4>Você não possui nenhuma categoria cadastrada</h4>";
+    else {
+        foreach($cats as $c){
+            echo "<p>" . $c->id . " - " . $c->nome . "</p>";
+            $produtos = $c->produtos;
+
+            if(count($produtos) > 0){
+                echo "<ul>";
+                foreach($produtos as $p){
+                    echo "<li>" . $p->nome . "</li>";
+                }
+                echo "</ul>";
+
+            }
+
+        }
+    }
+});
