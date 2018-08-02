211e4689846ee8579cdbea80fc474e537e0fca7e
Bruna Alves <brunahalves94@gmail.com>
2018-08-02 11:00:41 -0300

    Relacionamento UM PRA MUITOS: Eager loading

--- routes/web.php
+++ routes/web.php
@@ -719,3 +719,8 @@ Route::get('/categoriasprodutos', function(){
         }
     }
 });
+
+Route::get('/categoriasprodutos/json', function(){
+    $cats = Categoria::with('produtos')->get();
+    return $cats->toJson();
+});
