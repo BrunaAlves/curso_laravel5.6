fc52576c13fcf326b7a7a1bd9cdd24f2a96db777
Bruna Alves <brunahalves94@gmail.com>
2018-08-02 16:40:03 -0300

    Mudando nome padrão de tabela

--- app/Alocacao.php
+++ app/Alocacao.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alocacao extends Model
 {
-    //
+    protected $table = 'alocacoes'; //Caso não consiga seguir o padrão, é possivel falar qual é o nome correto da tabela
 }
