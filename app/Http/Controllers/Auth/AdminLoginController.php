eeba4db97b1309af0c73e8873322d8a9f48135f5
Bruna Alves <brunahalves94@gmail.com>
2018-08-15 16:28:55 -0300

    Login Multiusuário: Implementando verificação do login

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
 
 class AdminLoginController extends Controller
 {
@@ -13,7 +15,25 @@ class AdminLoginController extends Controller
     }
 
     public function login(Request $request){
-        return "OK";
+        
+       $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+        
+        $credentials =[
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        $authOK = Auth::guard('admin')->attempt($credentials, $request->remember);
+
+       if($authOK){
+            return redirect()->intended(route('admin.dashboard'));
+       }
+       
+       return redirect()->back()->withInputs($request->only('email', 'remember'));
+
     }
 
     public function index(){
